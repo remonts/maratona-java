feb074db8405e9c1575921a018e3c856c03bd358
Renan Monteiro <monteiro-renan@hotmail.com>
2023-01-23 17:43:32 -0300

    finalizado aula 22.

---
+++ src/renan/maratonajava/introducao/Aula05EstruturasCondicionais03.java
@@ -0,0 +1,16 @@
+package renan.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais03 {
+    public static void main(String[] args) {
+        // Doar se salario > 5000
+
+        double salario = 3000;
+        String mensagemDoar = "Eu vou doar 500 pro DevDojo";
+        String mensagemNaoDoar = "Ainda não tenho condições, mas vou ter!";
+
+        // (condicao) ? verdadeiro : falso;
+        String resultado = salario > 5000 ? mensagemDoar : mensagemNaoDoar;
+
+        System.out.println(resultado);
+    }
+}
