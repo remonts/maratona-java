5f5c32375b354f429318476a1bcb037cdce86c4a
Renan Monteiro <monteiro-renan@hotmail.com>
2023-01-23 10:34:32 -0300

    Aula 02.

--- src/renan/maratonajava/introducao/Aula01OlaMundoDevDojo.java
+++ src/renan/maratonajava/introducao/Aula01OlaMundoDevDojo.java
@@ -1,6 +1,6 @@
 package renan.maratonajava.introducao;
 
-public class OlaMundoDevDojo {
+public class Aula01OlaMundoDevDojo {
     public static void main(String[] args) {
         System.out.println("KA-ME-HA");
     }
--- src/renan/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/renan/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -1,2 +1,8 @@
-package renan.maratonajava.introducao;public class Aula02TiposPrimitivos {
+package renan.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        int idade = 10;
+        System.out.println("A idade é " + idade + " anos.");
+    }
 }
