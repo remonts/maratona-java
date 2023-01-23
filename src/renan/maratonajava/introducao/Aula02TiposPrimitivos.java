316eb24f4c923419edf209d0b5fa439b3ea7cfc7
Renan Monteiro <monteiro-renan@hotmail.com>
2023-01-23 11:20:01 -0300

    finalizado aula 02.

--- src/renan/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/renan/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -3,6 +3,20 @@ package renan.maratonajava.introducao;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         int idade = 10;
+        long numeroGrande = 100000L;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500.0F;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 65;
+        char caractere1 = '\u0042';
+        String nome = "Goku";
+
         System.out.println("A idade é " + idade + " anos.");
+        System.out.println("Char " + caractere);
+        System.out.println("Char " + caractere1);
+        System.out.println(nome);
     }
 }
