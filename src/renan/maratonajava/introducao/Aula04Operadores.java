29fbb2c852be62f446c301bf2d21ce470aba2a86
Renan Monteiro <monteiro-renan@hotmail.com>
2023-01-23 15:37:41 -0300

    finalizado Aula19.

--- src/renan/maratonajava/introducao/Aula04Operadores.java
+++ src/renan/maratonajava/introducao/Aula04Operadores.java
@@ -33,5 +33,24 @@ public class Aula04Operadores {
 
         boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
         System.out.println("isPlaystationCincoCompravel " + isPlaystationCincoCompravel);
+
+        // = += -= *= /= %=
+        double bonus = 1800;
+        bonus += 1000;
+        bonus -= 1000;
+        bonus *= 2;
+        bonus /= 2;
+        bonus %= 2;
+        System.out.println(bonus);
+
+        //
+        int contador = 0;
+        contador += 1; // contador = contador + 1;
+        contador++;
+        contador--;
+        ++contador;
+        --contador;
+        int contador2 = 0;
+        System.out.println(++contador2);
     }
 }
