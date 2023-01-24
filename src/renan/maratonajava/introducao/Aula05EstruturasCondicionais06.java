c3bd0588c80d8c3fe9e9569c9c929198454aa0a3
Renan Monteiro <monteiro-renan@hotmail.com>
2023-01-24 16:21:24 -0300

    Finalizado Aula 26.

---
+++ src/renan/maratonajava/introducao/Aula05EstruturasCondicionais06.java
@@ -0,0 +1,37 @@
+package renan.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais06 {
+    public static void main(String[] args) {
+        byte dia = 3;
+
+        switch (dia) {
+            case 1:
+            case 7:
+                System.out.println("Final de Semana");
+                break;
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                System.out.println("Dia Útil");
+                break;
+            default:
+                System.out.println("Opção inválida");
+                break;
+        }
+
+        char sexo = 'M';
+        switch (sexo) {
+            case 'M':
+                System.out.println("Homem");
+                break;
+            case 'F':
+                System.out.println("Mulher");
+                break;
+            default:
+                System.out.println("Inválido.");
+                break;
+        }
+    }
+}
