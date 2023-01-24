2d663a4677699b1aa43c7d0c19ee0b724531fe29
Renan Monteiro <monteiro-renan@hotmail.com>
2023-01-24 17:18:25 -0300

    Finalizado Aula 27.

---
+++ src/renan/maratonajava/introducao/Aula06EstruturasDeRepeticao01.java
@@ -0,0 +1,20 @@
+package renan.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao01 {
+    public static void main(String[] args) {
+        int count = 12;
+
+        while (count < 10) {
+            System.out.println(++count);
+        }
+
+        count = 0;
+        do {
+            System.out.println("dentro do do-while "+ ++count);
+        } while (count < 10);
+
+        for (int i = 0;i < 10; i++) {
+            System.out.println("For " + i);
+        }
+    }
+}
