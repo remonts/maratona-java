e0f93e6d507b8a49c7930ce4a799f41945b8998d
Renan Monteiro <monteiro-renan@hotmail.com>
2023-01-26 17:50:36 -0300

    Finalizado Aula 43 - Orientação Objetos - Métodos pt 01.

---
+++ src/renan/maratonajava/javacore/Bintroducaoclasses/dominio/Calculadora.java
@@ -0,0 +1,12 @@
+package renan.maratonajava.javacore.Bintroducaoclasses.dominio;
+
+public class Calculadora {
+
+    public void somaDoisNumeros() {
+        System.out.println(10 + 10);
+    }
+
+    public void subtraiDoisNumeros() {
+        System.out.println(21 - 2);
+    }
+}
---
+++ src/renan/maratonajava/javacore/Bintroducaoclasses/test/CalculadoraTest01.java
@@ -0,0 +1,13 @@
+package renan.maratonajava.javacore.Bintroducaoclasses.test;
+
+import renan.maratonajava.javacore.Bintroducaoclasses.dominio.Calculadora;
+
+public class CalculadoraTest01 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+        calculadora.somaDoisNumeros();
+        calculadora.subtraiDoisNumeros();
+
+    }
+}
