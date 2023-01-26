21c061fe523cd5ee5d3f3c6b6bc8eebdf7c58167
Renan Monteiro <monteiro-renan@hotmail.com>
2023-01-26 10:56:00 -0300

    Finalizado Aula 39 - Orientação Objetos pt 01 - Introdução classes pt 01.

---
+++ src/renan/maratonajava/javacore/introducaoclasses/dominio/Estudante.java
@@ -0,0 +1,7 @@
+package renan.maratonajava.javacore.introducaoclasses.dominio;
+
+public class Estudante {
+    public String nome;
+    public int idade;
+    public char sexo;
+}
---
+++ src/renan/maratonajava/javacore/introducaoclasses/test/EstudanteTest01.java
@@ -0,0 +1,16 @@
+package renan.maratonajava.javacore.introducaoclasses.test;
+
+import renan.maratonajava.javacore.introducaoclasses.dominio.Estudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.nome = "Renan";
+        estudante.idade = 29;
+        estudante.sexo = 'M';
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+    }
+}
