3da1b19d2ce34a1172fe0469042aff8bcc65809e
Renan Monteiro <monteiro-renan@hotmail.com>
2023-01-27 15:33:35 -0300

    Finalizado Aula 45 - Orientação Objetos - Métodos pt 02

---
+++ src/renan/maratonajava/javacore/Bintroducaoclasses/test/CalculadoraTest02.java
@@ -0,0 +1,11 @@
+package renan.maratonajava.javacore.Bintroducaoclasses.test;
+
+import renan.maratonajava.javacore.Bintroducaoclasses.dominio.Calculadora;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+        calculadora.multiplicaDoisNumeros(10, 20);
+    }
+}
