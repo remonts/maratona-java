d71ac719fb314656f2fd6726e79bdba6124ae75d
Renan Monteiro <monteiro-renan@hotmail.com>
2023-02-01 15:10:04 -0300

    Finalizado Aula 58 - Orientação Objetos - Construtores.

--- src/renan/maratonajava/javacore/Dconstrutores/dominio/Anime.java
+++ src/renan/maratonajava/javacore/Dconstrutores/dominio/Anime.java
@@ -1,6 +1,4 @@
-package renan.maratonajava.javacore.Csobrecargametodos.dominio;
-
-import java.security.PublicKey;
+package renan.maratonajava.javacore.Dconstrutores.dominio;
 
 public class Anime {
     private String nome;
@@ -8,20 +6,15 @@ public class Anime {
     private int episodios;
     private String genero;
 
-    public Anime () {
-
-    }
-
-    public void init (String nome, String tipo, int episodios) {
+    public Anime(String nome, String tipo, int episodios, String genero) {
+        System.out.println("Dentro do construtor");
         this.nome = nome;
         this.tipo = tipo;
         this.episodios = episodios;
-    }
-
-    public void init (String nome, String tipo, int episodios, String genero) {
-        this.init(nome, tipo, episodios);
         this.genero = genero;
     }
+
+    public Anime() {}
 
     public void imprime() {
         System.out.println(this.nome);
---
+++ src/renan/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -0,0 +1,12 @@
+package renan.maratonajava.javacore.Dconstrutores.test;
+
+import renan.maratonajava.javacore.Dconstrutores.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Akudama Drive", "TV", 12, "Ação");
+        Anime anime2 = new Anime();
+        anime.imprime();
+        anime2.imprime();
+    }
+}
