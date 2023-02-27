e9ae67a3f70ff57d6fbd0af799fcb084f563d507
Renan Monteiro <monteiro-renan@hotmail.com>
2023-02-27 16:49:24 -0300

    Finalizado Aula 64 - Orientação Objetos - Associação pt 01.

---
+++ src/renan/maratonajava/javacore/Gassociacao/test/JogadorTest01.java
@@ -0,0 +1,17 @@
+package renan.maratonajava.javacore.Gassociacao.test;
+
+import renan.maratonajava.javacore.Gassociacao.dominio.Jogador;
+
+public class JogadorTest01 {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("Pelé");
+        Jogador jogador2 = new Jogador("Romário");
+        Jogador jogador3 = new Jogador("Cafú");
+
+        Jogador[] jogadores = {jogador1, jogador2, jogador3};
+
+        for (Jogador jogador: jogadores) {
+            jogador.imprime();
+        }
+    }
+}
