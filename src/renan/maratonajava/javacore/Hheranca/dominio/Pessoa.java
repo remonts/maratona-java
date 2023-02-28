02fbb57833c42e1a0f9071453afd6badaa7a1ede
Renan Monteiro <monteiro-renan@hotmail.com>
2023-02-28 10:56:11 -0300

    Finalizado Aula 73 - Orientação Objetos - Heranca pt 03.

--- src/renan/maratonajava/javacore/Hheranca/dominio/Pessoa.java
+++ src/renan/maratonajava/javacore/Hheranca/dominio/Pessoa.java
@@ -1,9 +1,9 @@
 package renan.maratonajava.javacore.Hheranca.dominio;
 
 public class Pessoa {
-    private String nome;
-    private String cpf;
-    private Endereco endereco;
+    protected String nome;
+    protected String cpf;
+    protected Endereco endereco;
 
     public void imprime() {
         System.out.println(this.nome);
---
+++ src/renan/maratonajava/javacore/Hheranca/test/Funcionario2.java
@@ -0,0 +1,9 @@
+package renan.maratonajava.javacore.Hheranca.test;
+
+import renan.maratonajava.javacore.Hheranca.dominio.Pessoa;
+
+public class Funcionario2 extends Pessoa {
+    public void imprime() {
+        this.nome = "sasaricando";
+    }
+}
