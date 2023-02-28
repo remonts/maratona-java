f9ee973ade447d19d45a8eacd895f55e3a964434
Renan Monteiro <monteiro-renan@hotmail.com>
2023-02-28 17:38:41 -0300

    Finalizado Aula 76 - Orientação Objetos - Sobrescrita do método toString.

---
+++ src/renan/maratonajava/javacore/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package renan.maratonajava.javacore.Isobrescrita.test;
+
+import renan.maratonajava.javacore.Isobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr. Stone");
+        System.out.println(anime);
+    }
+}
